3f0c3cc123aade52f5ce113b577e9c5626967ca5
Ajesh Jagdish Meshram <meshram.ajesh@gmail.com>
2025-04-09 08:23:25 +0000

    feat: button open dialog

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,17 @@ import {
 	useEdgesState,
 	addEdge,
 } from '@xyflow/react';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 import { Button } from "@/components/ui/button"
 import '@xyflow/react/dist/style.css';
 
@@ -45,9 +56,26 @@ const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 export default function App() {
 	return (
 		<div className="flex flex-col items-center justify-center min-h-svh">
-			<h1 className='text-red-500'>Hello Hazler Node</h1>
+			<h1 className='text-red-500 font-bold'>Hello Hazler Node</h1>
 
-			<Button>Click me</Button>
+			<AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="outline">Show Dialog</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
 		</div>
 
 
