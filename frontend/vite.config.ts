a32a26241c331cbca0a660100e08323b4a35ee7b
Ajesh Jagdish Meshram <meshram.ajesh@gmail.com>
2025-04-14 12:16:43 +0000

    feat: router works

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,12 @@ import path from 'path';
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import proxyOptions from './proxyOptions';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [TanStackRouterVite({ target: 'react', autoCodeSplitting: true }), react(), tailwindcss()],
 	server: {
 		port: 8080,
 		host: '0.0.0.0',
