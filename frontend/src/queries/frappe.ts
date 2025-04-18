05733d67df572ee8caf8276b37ce84796af2b44e
Ajesh Jagdish Meshram <meshram.ajesh@gmail.com>
2025-04-18 10:28:18 +0000

    refactor: functions outside main hook

--- frontend/src/queries/frappe.ts
+++ frontend/src/queries/frappe.ts
@@ -19,38 +19,41 @@ export interface DocTypeQueryParams<DT> {
 }
 
 export function useDocType<DT>(doctype: string) {
-  const getListQueryOptions = (params: DocTypeQueryParams<DT>) => {
-    if (!params.fields) {
-      params.fields = ['name' as keyof DT];
-    }
-    return queryOptions({
-      queryKey: [doctype, 'list', params],
-      queryFn: (): Promise<Array<DT>> => {
-        return makeRequest({
-          type: 'document',
-          path: `${doctype}`,
-          params: { ...params, fields: JSON.stringify(params.fields) }
-        })
-      },
-      enabled: !!params
-    });
-  }
-  const getDocQueryOptions = (name: string) =>
-    queryOptions({
-      queryKey: [doctype, 'doc', name],
-      queryFn: (): Promise<DT> => {
-        return makeRequest(
-          {
-            type: 'document',
-            path: `${doctype}/${document}`
-          })
-      },
-      enabled: !!name
-    });
-
   return {
     useList: (params: DocTypeQueryParams<DT> = {}) =>
-      useQuery(getListQueryOptions(params)),
-    useDoc: (name: string) => useQuery(getDocQueryOptions(name)),
+      useQuery(getListQueryOptions<DT>(doctype, params)),
+    useDoc: (name: string) => useQuery(getDocQueryOptions<DT>(doctype, name)),
   };
+}
+function getListQueryOptions<DT>(
+  doctype: string,
+  params: DocTypeQueryParams<DT>,
+) {
+  if (!params.fields) {
+    params.fields = ['name' as keyof DT];
+  }
+  return queryOptions({
+    queryKey: [doctype, 'list', params],
+    queryFn: (): Promise<Array<DT>> => {
+      return makeRequest({
+        type: 'document',
+        path: doctype,
+        params: { ...params, fields: JSON.stringify(params.fields) },
+      });
+    },
+    enabled: !!params,
+  });
+}
+
+function getDocQueryOptions<DT>(doctype: string, name: string) {
+  return queryOptions({
+    queryKey: [doctype, 'doc', name],
+    queryFn: (): Promise<DT> => {
+      return makeRequest({
+        type: 'document',
+        path: `${doctype}/${name}`,
+      });
+    },
+    enabled: !!name,
+  });
 }
