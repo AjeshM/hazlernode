c820b47e36aabf5b904e60965ef05eca3ea89a1c
Ajesh Jagdish Meshram <meshram.ajesh@gmail.com>
2025-04-18 10:37:56 +0000

    refactor: narrower types

--- frontend/src/queries/frappe.ts
+++ frontend/src/queries/frappe.ts
@@ -6,6 +6,12 @@ type FilterObject<DT> = Record<
   number | string | Array<string | number>
 >; // TODO: can be more granular later
 
+type DocTypeName =
+  | 'Hazler Node'
+  | 'Hazler Workflow'
+  | 'Hazler Node Type'
+  | 'Hazler Workflow Execution Log';
+
 export interface DocTypeQueryParams<DT> {
   fields?: ReadonlyArray<keyof DT> | '*';
   filters?: FilterObject<DT>;
@@ -18,7 +24,7 @@ export interface DocTypeQueryParams<DT> {
   asDict?: boolean;
 }
 
-export function useDocType<DT>(doctype: string) {
+export function useDocType<DT>(doctype: DocTypeName) {
   return {
     useList: (params: DocTypeQueryParams<DT> = {}) =>
       useQuery(getListQueryOptions<DT>(doctype, params)),
@@ -45,7 +51,7 @@ function getListQueryOptions<DT>(
   });
 }
 
-function getDocQueryOptions<DT>(doctype: string, name: string) {
+function getDocQueryOptions<DT>(doctype: DocTypeName, name: string) {
   return queryOptions({
     queryKey: [doctype, 'doc', name],
     queryFn: (): Promise<DT> => {
@@ -57,12 +63,12 @@ function getDocQueryOptions<DT>(doctype: string, name: string) {
     enabled: !!name,
   });
 }
-export function useDocument<DT>(doctype: string, name: string) {
+export function useDocument<DT>(doctype: DocTypeName, name: string) {
   return useQuery(getDocQueryOptions<DT>(doctype, name));
 }
 
 export function useDocumentList<DT>(
-  doctype: string,
+  doctype: DocTypeName,
   params: DocTypeQueryParams<DT> = {},
 ) {
   return useQuery(getListQueryOptions<DT>(doctype, params));
