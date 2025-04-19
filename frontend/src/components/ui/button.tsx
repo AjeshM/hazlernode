869b2605739495e2a0452c4160cf6fe52ac0772f
Ajesh Jagdish Meshram <meshram.ajesh@gmail.com>
2025-04-19 15:53:03 +0000

    fix: href -> to in button

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -192,7 +192,7 @@ export const Button = React.forwardRef(function Button(
     outline ? styles.outline : plain ? styles.plain : clsx(styles.solid, styles.colors[color ?? 'dark/zinc'])
   )
 
-  return 'href' in props ? (
+  return 'to' in props ? (
     <Link {...props} className={classes} ref={ref as React.ForwardedRef<HTMLAnchorElement>}>
       <TouchTarget>{children}</TouchTarget>
     </Link>
