97aa851be001ea8762b3c0d87a459ad8f7147e36
Ajesh Jagdish Meshram <meshram.ajesh@gmail.com>
2025-04-22 02:43:28 +0000

    feat: delete workflow

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,7 +10,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 // Import the generated route tree
 import { routeTree } from './routeTree.gen';
 import { getSessionUserId } from './data/session';
-import { Skeleton } from './components/ui/skeleton';
+import { Skeleton } from '@/components/ui/skeleton';
 
 // Create a new router instance
 const router = createRouter({
--- frontend/src/routes/workflow.$id.tsx
+++ frontend/src/routes/workflow.$id.tsx
@@ -1,22 +1,44 @@
-import { useDocType } from "@/queries/frappe";
-import { createFileRoute } from "@tanstack/react-router";
-
+import { Button } from '@/components/ui/button';
+import { useDocType } from '@/queries/frappe';
+import { createFileRoute, useNavigate } from '@tanstack/react-router';
+import { toast } from 'sonner';
 
 export const Route = createFileRoute('/workflow/$id')({
   // loader: ({ context: { queryClient }, params: { id } }) => {
   //   return id
   // },
   component: WorkflowDetails,
-})
+});
 function WorkflowDetails() {
   const params = Route.useParams();
-  const { useSuspenseDoc } = useDocType<HazlerWorkflow>('Hazler Workflow');
+  const navigate = useNavigate();
+  const { useSuspenseDoc, useDeleteDocMutation } =
+    useDocType<HazlerWorkflow>('Hazler Workflow');
   const workflowDoc = useSuspenseDoc(params.id);
+  const deleteWorkflowMutation = useDeleteDocMutation();
+
+  function handleDeleteWorkflow() {
+    // todo: ask for confirmation
+    deleteWorkflowMutation.mutate({
+      name: params.id,
+    }),
+      {
+        onSuccess: () => {
+          navigate({
+            to: '/',
+          });
+          toast.success('Workflow deleted successfully!!!');
+        },
+      };
+  }
   return (
     <>
       <div>
         <pre>{JSON.stringify(workflowDoc.data, null, 2)}</pre>
+        <Button color="rose" onClick={handleDeleteWorkflow}>
+          Delete Workflow
+        </Button>
       </div>
     </>
   );
-};
+}
