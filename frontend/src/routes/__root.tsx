8d7c015b37501e094fd81bfc7d7f22baed300fbd
Ajesh Jagdish Meshram <meshram.ajesh@gmail.com>
2025-04-23 04:08:17 +0000

    chore: move dev tools position

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,20 +1,22 @@
 import * as React from 'react';
-import { Link, Outlet, createRootRouteWithContext } from '@tanstack/react-router';
+import {
+  Link,
+  Outlet,
+  createRootRouteWithContext,
+} from '@tanstack/react-router';
 import { Toaster } from '@/components/ui/sonner';
-import "@/index.css";
+import '@/index.css';
 // Dev Tools (does not get bundled in production)
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient } from '@tanstack/react-query';
 import { options as userQueryOptions } from '@/queries/user';
-
 
 export const Route = createRootRouteWithContext<{
   queryClient: QueryClient;
   sessionUser: string | null;
 }>()({
   beforeLoad: async ({ location, context }) => {
-
     if (!context.sessionUser) {
       window.location.href = '/login?redirect-to=' + location.pathname;
     }
@@ -25,13 +27,13 @@ export const Route = createRootRouteWithContext<{
   pendingComponent: () => <p>User Data loading pending...</p>,
   errorComponent: () => <p>User Data loading failed...</p>,
   component: RootComponent,
-})
+});
 
 function RootComponent() {
   return (
     <React.Fragment>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold text-gray-800">
+      <div className="flex gap-2 p-2">
+        <Link to="/" className="text-gray-800 [&.active]:font-bold">
           Hazler✨Node
         </Link>
       </div>
@@ -40,8 +42,8 @@ function RootComponent() {
         <Outlet />
       </div>
       <Toaster />
-      <TanStackRouterDevtools position="bottom-right" />
-      <ReactQueryDevtools buttonPosition="top-right" />
+      <TanStackRouterDevtools position="bottom-left" />
+      <ReactQueryDevtools buttonPosition="bottom-right" />
     </React.Fragment>
-  )
+  );
 }
