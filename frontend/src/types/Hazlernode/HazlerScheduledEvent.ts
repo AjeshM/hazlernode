472937bad26122b95bfc324bb50ba69278aeaae1
Ajesh Meshram <meshram.ajesh@gmail.com>
2025-05-13 03:05:02 +0000

    feat: scheduled event trigger!

---
+++ frontend/src/types/Hazlernode/HazlerScheduledEvent.ts
@@ -0,0 +1,17 @@
+
+export interface HazlerScheduledEvent{
+	name: string
+	creation: string
+	modified: string
+	owner: string
+	modified_by: string
+	docstatus: 0 | 1 | 2
+	parent?: string
+	parentfield?: string
+	parenttype?: string
+	idx?: number
+	/**	CRON Expression : Data	*/
+	cron_expression?: string
+	/**	Scheduled Job Type : Link - Scheduled Job Type	*/
+	scheduled_job_type?: string
+}
