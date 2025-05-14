d56041f65049d65106f2896b2cfb4cb825883342
Ajesh Meshram <meshram.ajesh@gmail.com>
2025-05-14 02:43:35 +0000

    chore: sample email node

---
+++ hazlernode/nodes/actions/email_node.py
@@ -0,0 +1,9 @@
+import frappe
+
+from hazlernode.nodes import Node
+
+
+class EmailNode(Node):
+    def execute(self, event, params=None, context=None):
+        # TODO: frappe.sendmail()
+        pass
